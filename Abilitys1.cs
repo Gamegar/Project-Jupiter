e4aac9b67aae4d6dac8a580027fd760ace81d76c
Gamegar <43215912+Gamegar@users.noreply.github.com>
2019-01-15 09:14:44 -0500

    Add files via upload

---
+++ Abilitys1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Abilitys1 : MonoBehaviour
+{
+    public GameObject Panel;
+    //create the varable for modification
+
+    void OpenPanel() {
+
+        if(Panel != null){
+            Panel.SetActive(true);
+        }
+
+    }
+}
