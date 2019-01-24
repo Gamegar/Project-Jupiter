f46f4554e33cef079e9d0c1ed2331d934746bc55
Gamegar <43215912+Gamegar@users.noreply.github.com>
2019-01-24 11:13:57 -0500

    Add files via upload

---
+++ Basicattack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basicattack : MonoBehaviour
+{
+    public int damage;
+    public GameObject enemy;
+
+    private void Update()
+    {
+     for (int i=0; i<0;  i++ ){}
+
+    // enemy.GetComponent<BStats>().health -= damage;
+        
+    }
+  
+}
