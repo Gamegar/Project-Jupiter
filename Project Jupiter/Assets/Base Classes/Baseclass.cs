3398987a80159e7344ac19c7aa7cf1165bea8352
Gamegar <43215912+Gamegar@users.noreply.github.com>
2019-09-22 14:41:56 -0400

    Create Baseclass.cs

---
+++ Project Jupiter/Assets/Base Classes/Baseclass.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Baseclass : MonoBehaviour
+{
+    public string TheName;
+    public float curHP;
+    public float baseHP;
+
+    
+    public float baseMP;
+    public float curMP;
+    public float BaseAttack;
+    public float Curattack;
+    public float BaseDefence;
+    public float CurDefence;
+
+   
+   
+}
