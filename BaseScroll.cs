45db5309f31f23af95d854329a85b06d44390328
Gamegar <43215912+Gamegar@users.noreply.github.com>
2019-09-22 14:41:31 -0400

    Create BaseScroll.cs

---
+++ BaseScroll.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseScroll : BaseItem
+{
+
+    private int spellEffectID;
+
+    public int SpellEffectID {
+
+        get { return spellEffectID; }
+        set { spellEffectID = value; }
+
+
+    }
+
+}
