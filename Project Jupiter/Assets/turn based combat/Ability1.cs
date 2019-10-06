e058cce84e5149addf57e24ff71ebd6d23c7f514
Gamegar <43215912+Gamegar@users.noreply.github.com>
2019-10-06 16:58:44 -0400

    Update Ability1.cs

--- Project Jupiter/Assets/turn based combat/Ability1.cs
+++ Project Jupiter/Assets/turn based combat/Ability1.cs
@@ -9,14 +9,13 @@ public class Ability1 : MonoBehaviour
     public GameObject enemy2;
     public GameObject enemy3;
     public GameObject enemy4;
-    private void Update()
-    {
-        for (int i = 0; i < 0; i++) { }
+    //private void Update()
 
-        enemy1.GetComponent<BStats>().health -= damage;
-        enemy2.GetComponent<BStats>().health -= damage;
-        enemy3.GetComponent<BStats>().health -= damage;
-        enemy4.GetComponent<BStats>().health -= damage;
+    public void enemies()
+    {
+        //for (int i = 0; i < 0; i++) { }
+
+       
 
     }
 
