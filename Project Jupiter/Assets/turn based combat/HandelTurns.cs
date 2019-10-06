0ad19db81ad51745c20fa29236eb9b4f42691dba
Gamegar <43215912+Gamegar@users.noreply.github.com>
2019-10-06 17:00:08 -0400

    Create HandelTurns.cs

---
+++ Project Jupiter/Assets/turn based combat/HandelTurns.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class HandelTurns
+{
+
+
+   public string Attacker; //name of attacker
+    public string Type;
+    public GameObject AttacksGameObject; //who is attacker
+    public GameObject AttackersTarget; // is attacked
+}
