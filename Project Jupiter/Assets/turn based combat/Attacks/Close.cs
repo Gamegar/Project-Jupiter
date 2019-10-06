96a53066c4c10bf6a5f4950a5e0110b44c6799b6
Gamegar <43215912+Gamegar@users.noreply.github.com>
2019-10-06 16:59:04 -0400

    Create Close.cs

---
+++ Project Jupiter/Assets/turn based combat/Attacks/Close.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Close : MonoBehaviour
+{
+    public GameObject Panel;
+    //create the varable for modification
+
+    public void ClosePanel()
+    {
+
+        if (Panel == true)
+        {
+            Panel.SetActive(false);
+        }
+
+    }
+
+
+}
