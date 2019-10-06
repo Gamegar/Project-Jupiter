1defb3ceac5e4c4a018e6b4be7a8e6cd03a22be3
Gamegar <43215912+Gamegar@users.noreply.github.com>
2019-10-06 16:52:23 -0400

    Update BaseCharacterClass.cs

--- Project Jupiter/Assets/Character Classes/BaseCharacterClass.cs
+++ Project Jupiter/Assets/Character Classes/BaseCharacterClass.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClassesPlayer {
+public class BaseCharacterClass
+{
     private string characterClassName;
 
     private string characterClassDescription;
