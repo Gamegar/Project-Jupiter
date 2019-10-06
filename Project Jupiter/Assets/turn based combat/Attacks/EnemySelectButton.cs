4269541309be5b1062474c9ac37d887f48b238b0
Gamegar <43215912+Gamegar@users.noreply.github.com>
2019-10-06 16:59:14 -0400

    Create EnemySelectButton.cs

---
+++ Project Jupiter/Assets/turn based combat/Attacks/EnemySelectButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySelectButton : MonoBehaviour
+{
+    public GameObject enemyPreFab;
+
+    public void SelectEnemy() {
+
+        GameObject.Find("BattleManager").GetComponent<BattleStateMachine>().Input2(enemyPreFab);
+    }
+}
